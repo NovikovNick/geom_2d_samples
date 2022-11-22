cfc48df87b5d424f46bccf9734927f0a617b86c2
nnovikov <nicknovikov10@gmail.com>
2022-11-22 18:34:01 +0300

    add several SFML tutorials

--- menu/main.cpp
+++ menu/main.cpp
@@ -1,46 +1,49 @@
-﻿#include <SFML/Graphics.hpp>
+﻿#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 #include <chrono>
 #include <iostream>
 #include <thread>
 
 #define GEOM_DEBUG 1;
 
+#include "src/cursor.cc"
 #include "src/resource_manager.h"
+#include "src/tile_map.cc"
 #include "src/util.h"
+
+// todo:
+// 1. snake board tile map
+// 2. menu screen => game screen => win/lose screen
+// 3. Screen transition + multithreading
 
 int main() {
   auto resource_mng = std::make_shared<geom_2d::ResourceManager>();
+  sf::ContextSettings settings;
+  settings.antialiasingLevel = 8;
 
   sf::RenderWindow window(sf::VideoMode(1000, 800), "Menu",
-                          sf::Style::Fullscreen);
+                          sf::Style::Fullscreen, settings);
 
   // Never use both setVerticalSyncEnabled and setFramerateLimit at the same
   // time!
-  // window.setVerticalSyncEnabled(true);
-  window.setFramerateLimit(24);
-
-  sf::CircleShape shape(10.f);
-  shape.setFillColor(sf::Color::Cyan);
-
-  sf::Font& font = resource_mng->GetFont();
+  window.setVerticalSyncEnabled(true);
+  // window.setFramerateLimit(60);
+  window.setMouseCursorVisible(false);
 
   // Initialize the pause message
-  sf::Text pauseMessage;
-  pauseMessage.setFont(font);
-  pauseMessage.setCharacterSize(40);
-  pauseMessage.setPosition(170.f, 150.f);
-  pauseMessage.setFillColor(sf::Color::Magenta);
-  pauseMessage.setString(L"Играть с компьютером\nВыход");
-  int count = 0;
+  sf::Text msg;
+  msg.setFont(resource_mng->GetFont());
+  msg.setCharacterSize(40);
+  msg.setString(L"Привет! ");
+  msg.setPosition(575, 56);
 
-  auto t1 = sf::milliseconds(15);
+  auto sprite_pos = sf::Vector2f(600, 120);
 
   sf::Sprite sprite;
   auto& texture = resource_mng->GetRunningSprite();
   texture.setSmooth(true);
   sprite.setTexture(texture);
-  sprite.setScale(sf::Vector2f(5.f, 5.f)); 
-
+  sprite.setPosition(sprite_pos);
 
   std::vector<sf::IntRect> anim_coords;
   for (int row = 0; row < 4; ++row) {
@@ -48,8 +51,41 @@ int main() {
       anim_coords.emplace_back(col * 75, row * 86, 75, 86);
     }
   }
+
+  sf::RectangleShape shape(sf::Vector2f(75.f, 86.f));
+  shape.setFillColor(sf::Color(100, 100, 100));
+  shape.setOutlineThickness(1.f);
+  shape.setOutlineColor(sf::Color::Magenta);
+  shape.setPosition(sprite_pos);
+
+  // cursor
+  geom_2d::Cursor cursor(30);
+  sf::SoundBuffer buffer;
+  if (!buffer.loadFromFile("resources/click.wav")) return -1;
+  sf::Sound sound;
+  sound.setBuffer(buffer);
+
+
+  // tile map
+  // define the level with an array of tile indices
+  const std::vector<int> level{
+      0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+      1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3,
+      3, 3, 3, 3, 0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0, 0, 1,
+      1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0, 0, 0, 1, 0, 3, 0, 2, 2,
+      0, 0, 1, 1, 1, 1, 2, 0, 2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1,
+      1, 1, 0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+  };
+
+  // create the tilemap from the level definition
+  geom_2d::TileMap map;
+  if (!map.load("resources/graphics-vertex-array-tilemap-tileset.png",
+                sf::Vector2u(32, 32), level, 16, 8))
+    return -1;
+
+  int fps = 1;
   int frame = 0;
-  //sf::Clock clock;  // starts the clock
+  // sf::Clock clock;  // starts the clock
   while (window.isOpen()) {
     auto t0 = std::chrono::steady_clock::now();
 
@@ -73,24 +109,38 @@ int main() {
           break;
         case sf::Event::MouseButtonPressed:
           if (event.mouseButton.button == sf::Mouse::Left) {
+            sound.play();
             std::cout << "the left button was pressed" << std::endl;
             std::cout << "mouse x: " << event.mouseButton.x << std::endl;
             std::cout << "mouse y: " << event.mouseButton.y << std::endl;
           }
           break;
         case sf::Event::MouseMoved:
-          sprite.setPosition(event.mouseMove.x, event.mouseMove.y);
-          std::cout << "new mouse x: " << event.mouseMove.x << std::endl;
-          std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
+          auto [x, y] = event.mouseMove;
+          cursor.setPosition(x, y);
+          msg.setFillColor(sf::Color(x % 255, y % 255, frame % 255));
+
+          auto bounds = cursor.getBounds();
+          shape.setFillColor(shape.getGlobalBounds().intersects(bounds)
+                                 ? sf::Color::Green
+                                 : sf::Color::Black);
+
+          geom_2d::debug("{:6d} Bounds are: {},{}, {} - {}\n", frame,
+                         bounds.left, bounds.top, bounds.width, bounds.height);
           break;
       }
     }
 
+    // animation
     sprite.setTextureRect(anim_coords[++frame % 27]);
 
     window.clear(sf::Color::Black);
-    window.draw(pauseMessage);
+    window.draw(msg);
+    window.draw(shape);
     window.draw(sprite);
+    window.draw(map);
+    window.draw(cursor);
+
     window.display();
 
     // geom_2d::debug("Elapsed {} ms\n", clock.restart().asMilliseconds());
---
+++ menu/src/tile_map.cc
@@ -0,0 +1,63 @@
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+
+namespace geom_2d {
+
+class TileMap : public sf::Drawable, public sf::Transformable {
+  sf::VertexArray vertices_;
+  sf::Texture tileset_;
+
+ public:
+  bool load(const std::string& tileset, const sf::Vector2u ts,
+            const std::vector<int>& tiles, const uint32_t width,
+            const uint32_t height) {
+    // load the tileset texture
+    if (!tileset_.loadFromFile(tileset)) return false;
+
+    // resize the vertex array to fit the level size
+    vertices_.setPrimitiveType(sf::Quads);
+    vertices_.resize(width * height * 4);
+
+    // populate the vertex array, with one quad per tile
+    for (uint32_t i = 0; i < width; ++i)
+      for (uint32_t j = 0; j < height; ++j) {
+        // get the current tile number
+        int tileNumber = tiles[i + j * width];
+
+        // find its position in the tileset texture
+        int tu = tileNumber % (tileset_.getSize().x / ts.x);
+        int tv = tileNumber / (tileset_.getSize().x / ts.x);
+
+        // get a pointer to the current tile's quad
+        sf::Vertex* quad = &vertices_[(i + j * width) * 4];
+
+        // define its 4 corners
+        quad[0].position = sf::Vector2f(i * ts.x, j * ts.y);
+        quad[1].position = sf::Vector2f((i + 1) * ts.x, j * ts.y);
+        quad[2].position = sf::Vector2f((i + 1) * ts.x, (j + 1) * ts.y);
+        quad[3].position = sf::Vector2f(i * ts.x, (j + 1) * ts.y);
+
+        // define its 4 texture coordinates
+        quad[0].texCoords = sf::Vector2f(tu * ts.x, tv * ts.y);
+        quad[1].texCoords = sf::Vector2f((tu + 1) * ts.x, tv * ts.y);
+        quad[2].texCoords = sf::Vector2f((tu + 1) * ts.x, (tv + 1) * ts.y);
+        quad[3].texCoords = sf::Vector2f(tu * ts.x, (tv + 1) * ts.y);
+      }
+
+    return true;
+  }
+
+ private:
+  virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    // apply the transform
+    states.transform *= getTransform();
+
+    // apply the tileset texture
+    states.texture = &tileset_;
+
+    // draw the vertex array
+    target.draw(vertices_, states);
+  }
+};
+
+};  // namespace geom_2d
